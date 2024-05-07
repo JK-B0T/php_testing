48c193ea310d7b8d21cfba320e414300c191612a
JK-B0T <JK-B0T@users.noreply.github.com>
2024-05-07 12:30:10 +0200

    Arregla ejemplo formulario

--- HermanitoChrish/registrar_usuario.php
+++ HermanitoChrish/registrar_usuario.php
@@ -4,18 +4,18 @@
     if ($conexion->connect_error) {
         die("Error de conexión: " . $conexion->connect_error);
     }
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = $_POST["user_name"];
-        $email = $_POST["user_email"];
-        $pass = $_POST["user_pass"];
     
-
-        $sql = "INSERT INTO USUARIO (nombre, email, contrasenya) VALUES ($name, $email, $pass)";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombre = $_POST["nombre"];
+        $email = $_POST["email"];
+        $contrasenya = $_POST["contrasenya"];
+    
+        $sql = "INSERT INTO USUARIO (nombre, email, contrasenya) VALUES ('$nombre', '$email', '$contrasenya')";
 
         if ($conexion->query($sql) === TRUE) {
             echo "Usuario registrado con éxito";
         } else {
-            echo "Error"
+            echo "Error" . $conexion->error;
         }
     }
     $conexion->close();
